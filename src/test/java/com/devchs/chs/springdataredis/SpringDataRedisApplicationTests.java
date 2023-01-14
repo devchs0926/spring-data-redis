c20830ae3acf006b88acc717fc017c6408d268c4
devchs0926 <devchs0926@gmail.com>
2023-01-15 02:12:09 +0900

    initial commit

---
+++ src/test/java/com/devchs/chs/springdataredis/SpringDataRedisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devchs.chs.springdataredis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataRedisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
